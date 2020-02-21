bd15e26b1df9fe4442d37027ff0c33e54fd03234
muhmuhcow <chrismuh90@gmail.com>
2020-02-21 23:51:45 +1100

    linked node to database

--- server/app.js
+++ server/app.js
@@ -6,18 +6,32 @@ app.use(express.static(__dirname + '/views'));
 app.set('views', path.join(__dirname, 'views'));
 const PORT = process.env.PORT || 5000;
 //const routes = require('./routes/routes.js');
-//const db = require('./database/db.js');
+const db = require('./database/db.js');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
     extended: true
   }));
+var userSchema = require("./database/userSchema.js");
+
 
 app.listen(PORT,()=>console.log(`listening on ${PORT}`));
 
 app.get("/",function(req,res){
+    
     res.send("yo wuddup");
 }); 
 
 app.get("/getStuff",function(req,res){
     res.send({stuff : "myStuff"});
+}); 
+
+
+app.get("/addShitToDatabase",function(req,res){
+    var newUser = new userSchema();
+    newUser.username = "Bob";
+    newUser.score = "12";
+    newUser.save(function(err,data){
+        if(err){console.log(err);}
+        console.log("success!");
+    });
 }); 
---
+++ server/database/userSchema.js
@@ -0,0 +1,14 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://andrews-team:ilovemilk123@cluster0-shard-00-00-vpase.mongodb.net:27017,cluster0-shard-00-01-vpase.mongodb.net:27017,cluster0-shard-00-02-vpase.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+var userSchema = new Schema({
+     "username": String,
+     "score": String
+   });
+
+ var userModel = mongoose.model("userModel",userSchema);
+
+ module.exports = userModel;
