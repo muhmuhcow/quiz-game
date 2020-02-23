7e753421de1b9cadefe399aa2070dd63b9976f5f
muhmuhcow <chrismuh90@gmail.com>
2020-02-23 22:57:57 +1100

    add axios post request

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ app.use(express.static(__dirname + '/views'));
 app.set('views', path.join(__dirname, 'views'));
 const PORT = process.env.PORT || 5000;
 //const routes = require('./routes/routes.js');
-const db = require('./database/db.js');
+//const db = require('./database/db.js');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
     extended: true
   }));
-var userSchema = require("./database/userSchema.js");
+//var userSchema = require("./database/userSchema.js");
 
 
 app.listen(PORT,()=>console.log(`listening on ${PORT}`));
@@ -26,18 +26,18 @@ app.get("/getStuff",function(req,res){
 }); 
 
 
- app.get("/addShitToDatabase",function(req,res){
-     var newUser = new userSchema();
-     newUser.username = "Bob";
-     newUser.score = "12";
-     newUser.save(function(err,data){
-         if(err){console.log(err);}
-         console.log("success!");
-     });
- }); 
+//  app.get("/addShitToDatabase",function(req,res){
+//      var newUser = new userSchema();
+//      newUser.username = "Bob";
+//      newUser.score = "12";
+//      newUser.save(function(err,data){
+//          if(err){console.log(err);}
+//          console.log("success!");
+//      });
+//  }); 
 
  app.post("/quizData",function(req,res){
-     //console.log("post request caught!")
+     console.log("post request caught!")
      console.log(req.body)
      //res.redirect('/') 
      res.send("finished!") 
